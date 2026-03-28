ec22ca33bf8f5b9ee9225098927cb6e0cdcf6dbf
primakas <andreyprimak903@gmail.com>
2026-03-28 14:20:58 +0300

    +

--- bot.py
+++ bot.py
@@ -1,46 +1,28 @@
 import os
 import asyncio
-# import logging
+
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-
-# # Логирование
-# logging.basicConfig(level=logging.INFO)
-# logger = logging.getLogger(__name__)
 
 load_dotenv()  # Загрузка переменных из .env файла
 
 TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
     raise ValueError("BOT_TOKEN не установлен в переменных окружения")
+print(TOKEN) 
 
-# Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-
-
-# Обработчик команды /start
-@dp.message(Command("start"))
-async def start_handler(message: Message):
-    await message.answer("Привет! Я эхобот. Отправь мне любое сообщение, и я его повторю.")
-
-
-# Обработчик команды /help
-@dp.message(Command("help"))
-async def help_handler(message: Message):
-    await message.answer("Я просто повторяю твои сообщения. Отправь мне текст!")
-
 
 # Обработчик всех остальных сообщений (эхо)
 @dp.message()
 async def echo_handler(message: Message):
-    await message.answer(f"Ты написал: {message.text}")
+    await message.answer(message.text)
 
 
-async def main():    # Запуск бота
+async def main():  
     await dp.start_polling(bot)
 
 
